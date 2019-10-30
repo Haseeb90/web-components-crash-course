985afb7a091b2863e4fa4f1bb91036b4770c700c
Abdul Haseeb Khan <haseeb.abdul.khan@gmail.com>
2019-10-30 08:56:49 -0400

    Added the connectedCallback and constructor to toolbar element class

--- src/toolbar-component/toolbar.component.js
+++ src/toolbar-component/toolbar.component.js
@@ -1,10 +1,20 @@
 
 export default class AppToolbar extends HTMLElement {
+
+  constructor() {
+    super();
+    console.log('This is from the constructor');
+  }
+
   get title() {
     return this.getAttribute('title');
   }
 
   set title(value) {
     this.setAttribute('title', value);
+  }
+
+  connectedCallback() {
+    this.innerHTML = '<p>Yay! Finally something visual!</p>';
   }
 }
