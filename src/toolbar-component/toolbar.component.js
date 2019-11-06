4f3bb63ce625c820c4c9ff19a115fce2098ba001
Abdul Haseeb Khan <haseeb.abdul.khan@gmail.com>
2019-11-06 18:14:55 -0500

    Added example of document-fragment

--- src/toolbar-component/toolbar.component.js
+++ src/toolbar-component/toolbar.component.js
@@ -1,4 +1,17 @@
 
 export default class AppToolbar extends HTMLElement {
-  // logic for your component...
+  connectedCallback() {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < 3; i++) {
+      const paragraph = document.createElement('p');
+      paragraph.innerText = `This is paragraph ${i}`;
+
+      fragment.appendChild(paragraph);
+    }
+    console.log(`Content before appending: ${fragment.textContent}`);
+    const clone = fragment.cloneNode(true);
+    this.appendChild(clone);
+    console.log(`Content after appending: ${fragment.textContent}`);
+  }
 }
