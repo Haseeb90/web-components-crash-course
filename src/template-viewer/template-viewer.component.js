23f79569acbc4e200667260f49fc9d58fdb7303d
Abdul Haseeb Khan <haseeb.abdul.khan@gmail.com>
2019-11-06 18:45:55 -0500

    Added layout attribute and change detection logic

--- src/template-viewer/template-viewer.component.js
+++ src/template-viewer/template-viewer.component.js
@@ -1,4 +1,22 @@
 
 export default class TemplateViewer extends HTMLElement {
-  
+  static get observedAttributes() {
+    return ['layout'];
+  }
+
+  get layout() {
+    return this.getAttribute('layout');
+  }
+
+  set layout(value) {
+    this.setAttribute('layout', value);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'layout') {
+      const template = document.getElementById(newValue);
+      const clone = template.cloneNode(true);
+      this.appendChild(clone);
+    }
+  }
 }
