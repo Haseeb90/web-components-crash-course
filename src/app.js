8dd5c26e998f1fc9c0be3516a0fef519112ae1b7
Abdul Haseeb Khan <haseeb.abdul.khan@gmail.com>
2019-11-08 08:18:57 -0500

    Imported styles

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 
 import TemplateViewer from './template-viewer/template-viewer.component';
+import styles from '../styles.css';
 
 if (!customElements.get('template-viewer')) {
   customElements.define('template-viewer', TemplateViewer);
