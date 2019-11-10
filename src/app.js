4b57ea2e13ec67ab5d771566ae3272c279d2664e
Abdul Haseeb Khan <haseeb.abdul.khan@gmail.com>
2019-11-10 11:40:08 -0500

    Added event listener to the drop down.

--- src/app.js
+++ src/app.js
@@ -5,3 +5,10 @@ import styles from '../styles.css';
 if (!customElements.get('template-viewer')) {
   customElements.define('template-viewer', TemplateViewer);
 }
+
+const templateSelector = document.getElementById('template-selector');
+const templateViewer = document.querySelector('template-viewer');
+templateSelector.addEventListener('change', (event) => {
+  const valueSelected = event.target.value;
+  templateViewer.layout = valueSelected;
+});
